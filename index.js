850767ef0ff565142aa471cc8790c4ea83ea696e
lpdft <772346730@qq.com>
2019-12-17 20:26:08 +0800

    [ruyue] feat-w3c2-2: add crash change color from yellow to blue

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var move = document.getElementsByClassName("move")[0]; //获取元素
+var stick = document.getElementsByClassName("stick")[0];
+
 var x, y; //鼠标相对与div左边，上边的偏移
 var isDrop = false; //移动状态的判断鼠标按下才能移动
+//得到stick的left和top
+
+var stickX = stick.offsetLeft;
+var stickY = stick.offsetTop;
+
 move.onmousedown = function(e) {
   var e = e || window.event; //要用event这个对象来获取鼠标的位置
   x = e.clientX - move.offsetLeft;
   y = e.clientY - move.offsetTop;
-  isDrop = true; //设为true表示可以移动
+  isDrop = true; 
 }
 
 document.onmousemove = function(e) {
@@ -18,15 +25,38 @@ document.onmousemove = function(e) {
       var maxX = document.documentElement.clientWidth - move.offsetWidth;
       var maxY = document.documentElement.clientHeight - move.offsetHeight;
 
-      moveX=Math.min(maxX, Math.max(0,moveX));
-      moveY=Math.min(maxY, Math.max(0,moveY));
+      //是否重合的判断及改变颜色
+      var moveBoxX = move.offsetLeft;
+      var moveBoxY = move.offsetTop;
+      var isIntersect = (moveBoxX >= stickX - 80 && moveBoxX <= stickX + 80) 
+                     && (moveBoxY >= stickY - 80 && moveBoxY <= stickY + 80)
+      //console.log(isIntersect);
+      if(isIntersect) {
+        stick.style.backgroundColor = "blue";
+      }
+      else {
+        stick.style.backgroundColor = "yellow";
+      }
+
+      if(moveX < 0) {
+          moveX = 0
+      } else if(moveX > maxX) {
+          moveX = maxX;
+      }
+
+      if(moveY < 0) {
+          moveY = 0;
+      } else if(moveY > maxY) {
+          moveY = maxY;
+      }
       move.style.left = moveX + "px";　　
-      move.style.top = moveY + "px";　　　　　　　　　　
+      move.style.top = moveY + "px";　　　　　　　　　
   } 
   else {
       return;　　　　　　　　　　
   }
 }
+
 document.onmouseup = function() {
-  isDrop = false; //设置为false不可移动
+  isDrop = false;
 }
